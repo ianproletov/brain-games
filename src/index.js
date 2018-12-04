65dc1507492f5197eea2fdee5b02e9fcbba85291
Ian Proletov <yan.proletov@gmail.com>
2018-12-04 17:49:04 +0300

    babelrc targets mistake

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 
 export const question = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log('Hello, ' + userName');
+  console.log('Hello, ' + userName);
   return;
 };
