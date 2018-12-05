1223ff4ad4f4459b8bbea5170c3c52b373b85310
Ian Proletov <yan.proletov@gmail.com>
2018-12-05 20:07:01 +0300

    brain eval

--- src/bin/brain-eval.js
+++ src/bin/brain-eval.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import { question } from '..';
+import { evalGame } from '..';
 
 console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".\n');
-question();
+evalGame();
--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import readlineSync from 'readline-sync';
 
 export const question = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}`);
+  console.log(`Hello, ${userName}\n`);
   return userName;
 };
 
 const randomNumber = () => Math.floor(Math.random() * (99 - 1)) + 1;
 
 const correct = (answer, number) => {
-  if (answer === 'yes' && number % 2 === 0) {
+  const corAnswer = number % 2 === 0 ? 'yes' : 'no';
+  if (answer === corAnswer) {
+    console.log('Correct!');
     return true;
   }
-  if (answer === 'no') {
-    return true;
-  }
+  console.log(`'${answer}' is wrong answer ;(. Correct answer was '${corAnswer}'`);
   return false;
 };
 
 const evalQuestion = () => {
   const questionNumber = randomNumber();
-  const answer = readlineSync.question(`Question: ${questionNumber}`);
+  const answer = readlineSync.question(`Question: ${questionNumber}\nYour answer: `);
   return correct(answer, questionNumber);
 };
 
@@ -28,13 +28,14 @@ export const evalGame = () => {
   const userName = question();
   const iter = (acc) => {
     if (acc === 3) {
-      console.log(`Congratiulations, ${userName}`);
+      console.log(`Congratiulations, ${userName}!`);
+      return null;
     }
     if (evalQuestion()) {
       return iter(acc + 1);
     }
-    console.log()
-    return 
+    console.log(`Let's try again, ${userName}!`);
+    return null;
   };
   return iter(0);
 };
